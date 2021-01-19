115611f35bad579870a166d01d0011916c6d98f8
dazelpro <dazelpro@gmail.com>
2021-01-19 14:23:04 +0700

    RUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -10,23 +11,22 @@ export class AppComponent {
 
     title = 'angular-firestore';
     myData: any[] = [];
+    id;
     nama;
     stok;
     harga;
 
+    isEdit;
+    
     constructor( private firestore: AngularFirestore ){
         this.tampilData();
     }
 
     tampilData() {
-        this.firestore.collection("barang")
-        .get()
-        .subscribe((ss) => {
-            ss.docs.forEach((doc) => {
-                this.myData.push(doc.data());
-            });
-            console.log(this.myData)
-        });
+        let data = this.firestore.collection('barang');
+        let dataTerbaru = data.valueChanges({ idField: 'id' });
+        dataTerbaru.subscribe(ss => this.myData = ss);
+        this.isEdit = false
     }
 
     async simpan() {
@@ -40,9 +40,40 @@ export class AppComponent {
         .then(res => {
             console.log(res);
             this.tampilData();
+            this.reset();
         })
         .catch(e => {
             console.log(e);
         })
+    }
+
+    getEdit(arr) {
+        this.isEdit = true;
+        this.id = arr.id;
+        this.nama = arr.namaBarang;
+        this.stok = arr.stokBarang;
+        this.harga = arr.hargaBarang;
+    }
+
+    edit() {
+        let data = {
+            namaBarang: this.nama,
+            stokBarang: this.stok,
+            hargaBarang: this.harga
+        }
+        this.firestore.collection('barang').doc(this.id).update(data);
+        this.tampilData();
+        this.reset();
+    }
+
+    delete(arr) {
+        this.firestore.collection('barang').doc(arr.id).delete();
+    }
+
+    reset() {
+        this.isEdit = false;
+        this.nama = "";
+        this.stok = "";
+        this.harga = "";
     }
 }
